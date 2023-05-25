202b388750a9e5ef8555f6e82d78a6257dade1a2
OPONY\iilnitskyi <ivan.ilnitskyi@oponeo.pl>
2023-05-25 14:21:00 +0200

    create PurchaseService and its implementation

---
+++ src/main/java/com/ilnitskyi/springbootproductseller/services/PurchaseService.java
@@ -0,0 +1,12 @@
+package com.ilnitskyi.springbootproductseller.services;
+
+import com.ilnitskyi.springbootproductseller.model.Purchase;
+import com.ilnitskyi.springbootproductseller.repository.projection.PurchaseItem;
+
+import java.util.List;
+
+public interface PurchaseService {
+    Purchase savePurchase(Purchase purchase);
+
+    List<PurchaseItem> findPurchaseItemOfUser(Long userId);
+}
---
+++ src/main/java/com/ilnitskyi/springbootproductseller/services/PurchaseServiceImpl.java
@@ -0,0 +1,28 @@
+package com.ilnitskyi.springbootproductseller.services;
+
+import com.ilnitskyi.springbootproductseller.model.Purchase;
+import com.ilnitskyi.springbootproductseller.repository.PurchaseRepository;
+import com.ilnitskyi.springbootproductseller.repository.projection.PurchaseItem;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PurchaseServiceImpl implements PurchaseService {
+    public final PurchaseRepository purchaseRepository;
+
+    @Override
+    public Purchase savePurchase(Purchase purchase) {
+        purchase.setPurchaseTime(LocalDateTime.now());
+
+        return purchaseRepository.save(purchase);
+    }
+
+    @Override
+    public List<PurchaseItem> findPurchaseItemOfUser(Long userId) {
+        return purchaseRepository.findAllPurchaseOfUser(userId);
+    }
+}
