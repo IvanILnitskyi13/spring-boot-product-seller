78461d39a5511f47eb58148181bb0ce7e48560ba
OPONY\iilnitskyi <ivan.ilnitskyi@oponeo.pl>
2023-05-25 11:45:59 +0200

    add database properties and create User entity and Role enum

---
+++ src/main/java/com/ilnitskyi/springbootproductseller/model/Role.java
@@ -0,0 +1,6 @@
+package com.ilnitskyi.springbootproductseller.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/ilnitskyi/springbootproductseller/model/User.java
@@ -0,0 +1,30 @@
+package com.ilnitskyi.springbootproductseller.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "create_time")
+    private LocalDateTime createTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+}
