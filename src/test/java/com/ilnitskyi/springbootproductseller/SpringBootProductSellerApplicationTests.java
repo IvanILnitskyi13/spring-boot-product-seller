cf7f742e4c67b9fde9aa3c2de21ba67c43d2ee83
OPONY\iilnitskyi <ivan.ilnitskyi@oponeo.pl>
2023-05-25 11:11:19 +0200

    init project

---
+++ src/test/java/com/ilnitskyi/springbootproductseller/SpringBootProductSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilnitskyi.springbootproductseller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProductSellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
