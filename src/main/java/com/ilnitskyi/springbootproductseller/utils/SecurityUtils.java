e558d688fb20f0a9470fd34cfc42f8c064c52b58
OPONY\iilnitskyi <ivan.ilnitskyi@oponeo.pl>
2023-05-29 10:55:06 +0200

    Load User by Username - User Details Service

---
+++ src/main/java/com/ilnitskyi/springbootproductseller/utils/SecurityUtils.java
@@ -0,0 +1,13 @@
+package com.ilnitskyi.springbootproductseller.utils;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public class SecurityUtils {
+    public static final String ROLE_PREFIX = "ROLE_";
+
+    public static SimpleGrantedAuthority convertToAuthority(String role) {
+        String formattedRole = role.startsWith(ROLE_PREFIX) ? role : ROLE_PREFIX.concat(role);
+
+        return new SimpleGrantedAuthority(formattedRole);
+    }
+}
